4410504cae53ad701ad4c41f8d97e5c414573334
No One <tqdatqn01230@gmail.com>
2023-08-10 23:28:14 +0700

    part of Efcore

---
+++ DataLayer/Models/JobPost.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataLayer.Models
+{
+    public class JobPost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public TypeOfWork Type;
+    }
+}
